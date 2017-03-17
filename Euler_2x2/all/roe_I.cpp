992350cee662d4d1972fe4a9a59182c8e092b80b
Cami Fiorini <cami@dhcp-193-51-32-118.math.uvsq.fr>
2017-03-17 12:02:28 +0100

    all/roe_I: parallel code working.

--- Euler_2x2/all/roe_I.cpp
+++ Euler_2x2/all/roe_I.cpp
@@ -30,39 +30,64 @@ void roe_I::compute_residual(vector<vector<double> >& R) const
 	for (int k = 0; k < 4; ++k)
 		R[k].assign(N,0);
 	
-	vector<double> UL(4), UR(4), Ustar(4);
-	
-	for (int i = 0; i <= N; ++i)
+	#pragma omp parallel default(shared)
 	{
-		int left = max(i-1, 0);
-		int right = min(i, N-1);
-		this->get_U(UL, left);
-		this->get_U(UR, right);
-		double lambda = this->compute_lambda(UL, UR);
+		int nt = omp_get_num_threads();
+		int tid = omp_get_thread_num();
+		int ChunkSize = N/nt;
+		int CS = ChunkSize;
+		if (tid == nt-1)
+			ChunkSize = N/nt + N%nt + 1;
+		vector<double> to_add(4, 0);
+		vector<double> UL(4), UR(4), Ustar(4);
 		
-		this->compute_U_star(UL, UR, Ustar);
-		if (CD)
+		for (int ip = 0; ip < ChunkSize; ++ip)
 		{
-			for (int k=0; k<4; ++k)
+			int i = ip + tid*CS;
+			int left = max(i-1, 0);
+			int right = min(i, N-1);
+			this->get_U(UL, left);
+			this->get_U(UR, right);
+			double lambda = this->compute_lambda(UL, UR);
+			
+			this->compute_U_star(UL, UR, Ustar);
+			if (CD)
 			{
-				if (i < N)
-					R[k][i] += lambda*(Ustar[k]-UR[k]) - sigma[i]*Ustar[k];
-				
-				if (i > 0)
-					R[k][i-1] += lambda*(Ustar[k]-UL[k]) + sigma[i]*Ustar[k];
+				for (int k=0; k<4; ++k)
+				{
+					if (i < N)
+						R[k][i] += lambda*(Ustar[k]-UR[k]) - sigma[i]*Ustar[k];
+					
+					if (i > 0)
+					{
+						if(ip == 0)
+							to_add[k] += lambda*(Ustar[k]-UL[k]) + sigma[i]*Ustar[k];
+						else
+							R[k][i-1] += lambda*(Ustar[k]-UL[k]) + sigma[i]*Ustar[k];
+					}
+				}
+			}
+			else
+			{
+				for (int k=0; k<4; ++k)
+				{
+					if (i < N)
+						R[k][i] += lambda*(Ustar[k]-UR[k]);
+					
+					if (i > 0)
+					{
+						if(ip == 0)
+							to_add[k] += lambda*(Ustar[k]-UL[k]);
+						else
+							R[k][i-1] += lambda*(Ustar[k]-UL[k]);
+					}
+				}
 			}
 		}
-		else
-		{
-			for (int k=0; k<4; ++k)
-			{
-				if (i < N)
-					R[k][i] += lambda*(Ustar[k]-UR[k]);
-				
-				if (i > 0)
-					R[k][i-1] += lambda*(Ustar[k]-UL[k]);
-			}
-		}
+		if (tid != 0)
+			for (int k = 0; k < 4; ++k)
+				R[k][tid*CS-1] += to_add[k];
+		
 	}
 	return;
 };
